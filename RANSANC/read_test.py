be7ef72465fed784a8a79c36e97e13f06227e538
Yushuo Feng <66173381+FullToker@users.noreply.github.com>
2024-11-10 15:16:21 +0100

    read test, cannot show the result?

---
+++ RANSANC/read_test.py
@@ -0,0 +1,38 @@
+from main import (
+    readCloud,
+    colorInlierOutlier,
+    cloudVisualizer,
+    keypoints2spheres,
+    progressbar,
+)
+
+
+pc_in = "/Volumes/T7 Shield/AdvancedGIS/read_test/"
+pc_out = ""
+filename = "01164_GE006.ply"
+
+winDims = [2000, 2000]
+viewCtrl = [0, 0, 1, 0, -1, 0, 400.01, 0.00, 10, 0.08]
+
+# Load Pointcloud
+cloud_in = readCloud(pc_in, filename)
+
+# Estimate Normals
+cloud_in.estimate_normals()
+
+# Perform RANSAC to find largest planar patch
+# Set values
+distance_threshold = 0.01
+ransac_n = 3
+num_iterations = 1000
+
+plane_model, inliers = cloud_in.segment_plane(
+    distance_threshold, ransac_n, num_iterations
+)
+
+# Set different colors for the found planar patch vs. rest of the cloud
+cloud_colored = colorInlierOutlier(cloud_in, inliers)
+
+# Visualize colored result
+viewTitle = "RANSAC"
+cloudVisualizer([cloud_colored], viewTitle, winDims, viewCtrl, 1)
